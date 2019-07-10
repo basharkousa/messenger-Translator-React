b3c9bc9f2bebec4a682105cf88a6681318512fb9
Bashar Kousa <basharkousax@gmail.com>
2019-07-10 17:09:27 +0300

    Updates

---
+++ precache-manifest.0a61d8ad6cccb027b6f4a813fc1e7e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a06edff78ae96c9632706b6706206d",
+    "url": "/basharkousa/messenger-Translator-React/index.html"
+  },
+  {
+    "revision": "8e6255de65801dceb8b8",
+    "url": "/basharkousa/messenger-Translator-React/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/basharkousa/messenger-Translator-React/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8e6255de65801dceb8b8",
+    "url": "/basharkousa/messenger-Translator-React/static/js/main.97d7661a.chunk.js"
+  },
+  {
+    "revision": "f1c036ce5d1eda17457d",
+    "url": "/basharkousa/messenger-Translator-React/static/js/runtime~main.41481437.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basharkousa/messenger-Translator-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
